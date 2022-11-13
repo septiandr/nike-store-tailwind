66bf10140139bd2e2b1cbafd1f8b711de536fe53
septiandr <sdwirisanggalih@gmail.com>
2022-11-13 14:00:01 +0700

    finish interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import Hero from './components/Hero'
-import { heroapi, popularsales, toprateslaes } from './data/data'
+import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data'
 import Sales from './components/Sales'
+import FlexContent from './components/FlexContent'
+import Stories from './components/Stories'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
       <main className='flex flex-col gap-16 relative '>
         <Hero heroapi={heroapi} />
-        <Sales endpoint={popularsales} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={toprateslaes} />
-      </main> 
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
     </div>
   )
 }
